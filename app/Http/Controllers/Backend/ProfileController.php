0aef2dde0bcb6b8bec4c11ccd99c1d61cd12bb95
ShehanLahiru <s.lahiru1995@gmail.com>
2020-05-09 19:21:14 +0530

    User done

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\User;
+use App\Helpers\APIHelper;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    /**
+     * Show the form for editing the profile.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit()
+    {
+        return view('backend.profile.edit');
+    }
+
+    /**
+     * Update the profile
+     *
+     * @param  \App\Http\Requests\ProfileRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request)
+    {
+        auth()->user()->update($request->all());
+
+        return back()->withStatus(__('Profile successfully updated.'));
+    }
+
+    /**
+     * Change the password
+     *
+     * @param  \App\Http\Requests\PasswordRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function password(Request $request)
+    {
+        auth()->user()->update(['password' => Hash::make($request->get('password'))]);
+
+        return back()->withPasswordStatus(__('Password successfully updated.'));
+    }
+}
